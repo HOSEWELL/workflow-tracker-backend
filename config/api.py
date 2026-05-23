6b5e925739917ae22da6ee3f2ef7cb4b389d2c10
HOSEWELL <hosewellkaranja@gmail.com>
2026-05-24 02:48:46 +0300

    swagger renaming

--- config/api.py
+++ config/api.py
@@ -1,6 +1,10 @@
 from ninja import NinjaAPI
 from applications.api import router as app_router
 
-api = NinjaAPI()
+api = NinjaAPI(
+    title="Workflow Tracker API",
+    version="1.0.0",
+    description="Workflow Tracker Backend APIs"
+)
 
 api.add_router("/applications/", app_router)
